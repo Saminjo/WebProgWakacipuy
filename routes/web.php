68c39fe04cdf545783b895467e04eb5c06bc703a
Karvin <108486623+KarvinNanda@users.noreply.github.com>
2022-10-26 11:32:50 +0700

    add web.php
    udah ada middleware dan juga perapian nama route nya

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\AuthController;
 use \App\Http\Controllers\ProductController;
+use \App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,14 +18,38 @@ use \App\Http\Controllers\ProductController;
 Route::get('/', function () {
     return view('home');
 });
-
+//login
 Route::get('/login', [AuthController::class, 'loginpage']);
 Route::post('/login', [AuthController::class, 'loginform']);
 
+//register
 Route::get('/register',[AuthController::class,'registerpage']);
 Route::post('/register',[AuthController::class,'registerform']);
 
+//logout
 Route::get('/logout',[AuthController::class,'logout']);
-Route::get('/showproduct',[ProductController::class,'index']);
 
+//show product
+Route::get('/show/product',[ProductController::class,'index'])->middleware('security');;
 
+//view product
+Route::get('/view/product',[ProductController::class,'viewProduct'])->middleware('security');
+
+//insert
+Route::get('/insert',[ProductController::class,'insertPage'])->middleware('security');
+Route::post('/insert',[ProductController::class,'insertProduct'])->middleware('security');
+
+//update
+Route::post('/update/{id}',[ProductController::class,'updatePage'])->middleware('security');
+Route::post('/update/item',[ProductController::class,'updateProduct'])->middleware('security');
+
+//delete
+Route::post('/delete/{id}',[ProductController::class,'deleteProduct'])->middleware('security');
+
+//edit profile
+Route::get('/edit/profile',[UserController::class,'editProfile'])->middleware('security');
+Route::post('/edit/profile',[UserController::class,'edit'])->middleware('security');
+
+//change password
+Route::get('/change/password',[UserController::class,'changePassword'])->middleware('security');
+Route::post('/change/password',[UserController::class,'change'])->middleware('security');
